17f061b71f00604bacd1a13349790553116b9d2f
div <119725613+Strel-k@users.noreply.github.com>
2024-05-27 14:17:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/index.css">
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+        <title>Portfolio</title>
+    </head>
+    <body>
+        
+        <section class="main-body">
+            <div class="center-body">
+                <h2>Need a Website or Program Developed?</h2>
+                <h2>Allow me to introduce myself!</h2>
+                <div class="main-body-information">
+                    <h4>Who am I?</h4>
+                    <p>I am F. Dave Damon V. Pararuan currently studying in Central Luzon State University (CLSU), 
+                    under the Bachelor of Science in Information Technology (BSIT) Program.
+                    My academic journey and hands-on projects has allowed me to develop my own skills in programming and web development especially.
+                <br> <br> I am incredibly well-versed with multiple languages such as C, Java, PHP, and JavaScript. I am also incredibly proficient in Database MySQL and HTML.</p>
+                    <div class="skills-information">
+                         <h4>Skills</h4>   
+                     <ul>
+                    <li data-info="I am fairly proficient with Web Development tools such as HTML, PHP, CSS, MySqL, and JavaScript.">
+                        <i class='bx bx-world'></i> Web Development
+                    </li>
+                    <li data-info="C is the language that I have started in. I highlight it because I believe it is a very flexible programming language.">
+                        <i class='bx bx-laptop'></i> C Programming
+                    </li>
+                    <li data-info="I am proficient in displaying the website in a aesthetically pleasing way.">
+                        <i class='bx bx-minus-front'></i> Front-End Development
+                    </li>
+                    <li data-info="I am familiar with Back-End Programming Languages such as PHP, JavaScript, and Python.">
+                        <i class='bx bx-minus-back'></i> Back-End Development
+                    </li>
+                    <li data-info="Database Management is one of my favorite subjects, as it is how data is stored.">
+                        <i class='bx bx-data'></i> Database Management
+                    </li>
+                    <li data-info="I am well-versed in the Object-Oriented Programming nature of Java and other OOP based Languages.">
+                    <i class='bx bxl-java' ></i> Java Programming
+                    </li>
+                    </ul>
+
+                    </div> 
+                    <h4>Projects Done</h4>  
+                    <div class="Projects">
+                        
+                        <div class="project-title">
+                        <i class='bx bxs-ship' ></i> <a href="https://github.com/Strel-k/Ferry-Reservation-Website">Binangkas Ferry Reservation System</a> 
+                        </div>
+                        <div class="image-projects-container">
+                            <img src="img/Binangkas.png" alt="">
+                        </div><br>
+                        <div class="project-title">
+                        <i class='bx bxl-java' ></i>  <a href="https://github.com/Strel-k/Coffee-Shop-Website-Full-stack">Coffee Shop Ordering System</a>
+                        </div>
+                        <div class="image-projects-container">
+                            <img src="img/Coffee Shop.png" alt="">
+                        </div><br>
+                        <div class="project-title">
+                        <i class='bx bxs-graduation' ></i> <a href="https://github.com/Strel-k/Finalized-University-Office-of-Admissions-">University Registrations</a>
+                        </div>
+                        <div class="image-projects-container">
+                            <img src="img/University-OAD.png" alt="">
+                        </div>
+                    </div>
+                 </div>
+            </div>
+        
+        </section>
+        <div class="image-container">
+                <img src="img/Portfolio-pic.jpg" alt="" style="border-radius:15px;">
+                <div class="image-text">
+                    <h4>F. Dave Damon V. Pararuan</h4>
+                    <h5>Contacts</h5>
+                <a href="https://www.facebook.com/fdave.pararuan"><i class='bx bxl-facebook-square'></i></a> 
+                    <a href=""><i class='bx bxl-linkedin-square' ></i></a>
+                    <a href=""><i class='bx bxs-envelope' ></i></a>
+                    <h5>Information</h5>
+
+                    <div class="information">
+                    <i class='bx bxs-briefcase-alt'><p>Student</p></i>
+                    <i class='bx bxs-city'><p>Cabanatuan City</p></i>
+                    <i class='bx bxs-flag'><p>Filipino</p></i>   
+                </div>
+                </div>  
+            </div>
+        <section class="footer">
+            Powered by div.
+        </section>
+    </body>
+    </html>
